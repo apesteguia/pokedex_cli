95104c4fc70959318f894049b6d6be713ff35f3b
apesteguia <mikeltxomikeltxo@gmail.com>
2023-11-17 21:39:22 +0100

    abilities

--- src/pkmn.rs
+++ src/pkmn.rs
@@ -1,30 +1,6 @@
 use reqwest;
 use serde::Deserialize;
 
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
-pub struct Pokemon {
-    name: String,
-    id: i32,
-    base_experience: i32,
-    types: Vec<Types>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
-struct Types {
-    //slot: i32,
-    #[serde(rename = "type")]
-    pokemon_type: Type,
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
-struct Type {
-    name: String,
-    //url: String,
-}
-
 pub const URL: &str = "https://pokeapi.co/api/v2/pokemon/";
 
 impl Pokemon {
@@ -37,7 +13,7 @@ impl Pokemon {
             let info = response.json::<Pokemon>().await?;
             Ok(info)
         } else {
-            println!("Ha habido un error");
+            println!("There has been an error, please check that the name or ID is correct.");
             Err(reqwest::Error::from(
                 response.error_for_status().unwrap_err(),
             ))
@@ -46,12 +22,55 @@ impl Pokemon {
     pub fn show(&self) {
         println!("");
         println!("POKEMON DATA");
-        println!("├─ id: {}", self.id);
-        println!("├─ name: {}", self.name);
-        println!("└─ types:");
+        println!("├─ Id: {}", self.id);
+        println!("├─ Name: {}", self.name);
+        println!("└─ Types:");
         for i in &self.types {
             println!("   └─ {}", i.pokemon_type.name);
         }
+        println!("└─ Abilities:");
+        for i in &self.abilities {
+            println!("   └─ {}", i.ability.name);
+        }
         println!("");
     }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Pokemon {
+    name: String,
+    id: u16,
+    base_experience: i32,
+    types: Vec<Types>,
+    abilities: Vec<Abilities>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Types {
+    //slot: u8,
+    #[serde(rename = "type")]
+    pokemon_type: Type,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Type {
+    name: String,
+    //url: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Abilities {
+    is_hidden: bool,
+    ability: Ability, //slot: u8,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Ability {
+    name: String,
+    //url: String,
 }
